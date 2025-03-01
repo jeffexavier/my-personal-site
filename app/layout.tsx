22904aba16b0e04f495124ed2aa2c59bfef912e8
Jefferson Xavier <jeffexavier@gmail.com>
2025-03-01 00:23:15 -0300

    Criação do arquivo do component da section e importado no layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
-import { Link } from "@heroui/link";
 import clsx from "clsx";
 
 import { Providers } from "./providers";
@@ -8,6 +7,7 @@ import { Providers } from "./providers";
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import NavbarTop from "@/components/navbar";
+import FooterSection from "@/components/footerSection";
 
 export const metadata: Metadata = {
   title: {
@@ -47,17 +47,7 @@ export default function RootLayout({
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
               {children}
             </main>
-            <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://heroui.com?utm_source=next-app-template"
-                title="heroui.com homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">HeroUI</p>
-              </Link>
-            </footer>
+            <FooterSection />
           </div>
         </Providers>
       </body>
---
+++ components/footerSection.tsx
@@ -0,0 +1,16 @@
+export default function FooterSection() {
+  return (
+    <>
+      {/* <Divider
+        className="mx-2 bg-white max-w-[1080px] rounded-full min-h-0.5"
+        orientation="horizontal"
+      /> */}
+      <div className="flex justify-center p-[210px]">
+        <div className="text-sm">
+          Site desenvolvido em Next.js com TypeScript por Jefferson Xavier
+          &copy; 2025
+        </div>
+      </div>
+    </>
+  );
+}
